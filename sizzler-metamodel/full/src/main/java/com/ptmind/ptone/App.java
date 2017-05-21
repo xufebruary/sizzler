a3115aab1e66aee9358f04a031d6fece45cf9177
peng.xu <809229821@qq.com>
2017-05-21 16:58:47 +0800

    add default file

---
+++ sizzler-metamodel/full/src/main/java/com/ptmind/ptone/App.java
@@ -0,0 +1,13 @@
+package com.ptmind.ptone;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
