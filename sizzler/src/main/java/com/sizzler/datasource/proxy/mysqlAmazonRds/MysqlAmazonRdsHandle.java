ffdde4cde6987e4fed5f32e889fcc57b37ef9651
peng.xu <809229821@qq.com>
2017-05-26 14:48:05 +0800

    增加oracle的demo数据源

---
+++ sizzler/src/main/java/com/sizzler/datasource/proxy/mysqlAmazonRds/MysqlAmazonRdsHandle.java
@@ -0,0 +1,38 @@
+package com.sizzler.datasource.proxy.mysqlAmazonRds;
+
+import com.sizzler.common.sizzler.UserConnection;
+import com.sizzler.datasource.proxy.RdsDataSourceHandle;
+import com.sizzler.domain.ds.dto.UserConnectionSourceDto;
+import com.sizzler.domain.ds.vo.UserConnectionSourceVo;
+import com.sizzler.domain.dto.UIEditorData;
+import com.sizzler.provider.common.UpdateDataResponse;
+
+
+/**
+ * for Oracle demo
+ */
+public class MysqlAmazonRdsHandle extends RdsDataSourceHandle {
+
+  @Override
+  public String getAccountSchema(UserConnection userConnection, String folderId) {
+    return super.getAccountSchema(userConnection, folderId);
+  }
+
+  @Override
+  public UserConnectionSourceVo pullRemoteData(UserConnection userConnection, String folderId, String fileId) {
+    return super.pullRemoteData(userConnection, folderId, fileId);
+  }
+
+  @Override
+  public UpdateDataResponse saveFileDataToDB(UserConnection userConnection,
+      UserConnectionSourceVo sourceVo, UserConnectionSourceDto sourceDto, 
+      Boolean isAutoUpdate) throws Exception {
+    return null;
+  }
+
+  @Override
+  public UIEditorData getEditData(UserConnection userConnection,
+      UserConnectionSourceDto sourceDto, Boolean isAutoUpdate, Boolean isUpdate) {
+    return super.getEditData(userConnection, sourceDto, isAutoUpdate, isUpdate);
+  }
+}
