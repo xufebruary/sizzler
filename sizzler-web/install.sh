215e79dd1e0ab83470b77be97e4da6979d964679
andy <andy@ptthink.com>
2017-06-21 15:14:46 +0800

    增加web前端的安装脚本install.sh

--- sizzler-web/install.sh
+++ sizzler-web/install.sh
@@ -6,3 +6,6 @@ npm install
 env=production npm run build
 
 zip -r dist.zip dist
+scp ./dist.zip  root@47.93.184.240:/opt/www
+ssh root@47.93.184.240 "rm -rf /opt/www/dist;unzip /opt/www/dist.zip -d /opt/www/"
+ssh root@47.93.184.240 "rm -rf /opt/www/sizzler;mv /opt/www/dist /opt/www/sizzler"
