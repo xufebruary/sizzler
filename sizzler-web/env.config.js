083488d0aa733197067d3e7330026ece3ffa9d46
andy <andy@ptthink.com>
2017-06-21 16:17:57 +0800

    添加test环境

--- sizzler-web/env.config.js
+++ sizzler-web/env.config.js
@@ -10,6 +10,16 @@ module.exports = {
 		'facebook.share.app.id.jp': '1546974012264908',
 		'facebook.share.app.id.com': '205599593197569'
 	},
+	test: {
+                'ptone.web.ui.url': 'http://192.168.202.71:9090',
+                'ptone.ptengine.url': '',
+                'ptone.websocket.url': '',
+                'ptone.web.middle.url': 'http://192.168.202.72:9999',
+                'ptone.web.middle.share.url': '',
+                'collect.server.url': '',
+                'facebook.share.app.id.jp': '1546974012264908',
+                'facebook.share.app.id.com': '205599593197569'
+        },
        production: {
                 'ptone.web.ui.url': 'http://47.93.184.240:9090',
                 'ptone.ptengine.url': '',
